5b9e373d73855b0012ec8482a781bf0fc6089a48
Valentin Prétat <deltabooq@gmail.com>
2020-10-31 21:09:31 +0100

    add broser support #2

--- website/static/website/js/browser-not-supported.js
+++ website/static/website/js/browser-not-supported.js
@@ -8,27 +8,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
         if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  
         {
             // alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
-            showBanner()
+            showAlert()
         }
         else  // If another browser, return 0
         {
             // alert('otherbrowser');
-            showBanner()
+            showAlert()
         }
     
         return false;
     }
 
-    function showBanner() {
+    function showAlert() {
         // Is it a great idea to have the html source in text format ?...
         document.querySelector("#scroller").appendChild(
             createElementFromHTML("" +
             "<div class=\"container\" style=\"position: sticky; bottom: 1rem;\">" +
-            "<div class=\"alert alert-primary\" role=\"alert\">" +
+            "<div class=\"alert alert-secondary\" role=\"alert\">" +
             "Internet Explorer n'est pas supporté par le site web. Pour son bon fonctionnement, veuillez utiliser un navigateur plus respectable !" +
+            "<input type=\"hidden\" value=\"コナミコマンド\">" +
+            "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">" +
+            "<span aria-hidden=\"true\">&times;</span>" +
+            "</button>" +
             "</div>" +
             "</div>"))
-        let support = new Konami(() => {toggleSupport()})
+        let support = new Konami(() => {addSupport()})
     }
 
     // Reference : https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro
@@ -37,14 +41,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
         div.innerHTML = htmlString.trim()
         // Change this to div.childNodes to support multiple top-level nodes
         return div.firstChild; 
-    }
-
-    function toggleSupport() {
-        addSupport()
-    }
-
-    function removeSupport() {
-
     }
 
     function addSupport() {

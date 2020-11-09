0fd34396a89532b39c796a0b02a2512472ebf134
Likyaz <louisphilemon.lavaud@gmail.com>
2020-11-09 23:36:12 +0100

    new regex email + branch get client ip

--- website/static/website/js/pages/contact.js
+++ website/static/website/js/pages/contact.js
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         document.querySelector("#contact-form").addEventListener("submit", function(evt) {
             evt.preventDefault()
 
-            trim()
+            strip()
 
             if(!validate()) {
                 return
@@ -110,9 +110,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
             // Email
             const email = document.querySelector("input[name=email]").value
+            // General Email Regex (RFC 5322 Official Standard)
+            const regex = /(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
             if(email == "")
                 returnValue.push(["input[name=email]", "L'email est requis"])
-            else if(email.match("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$") == null)
+            else if(email.match(regex) == null)
             returnValue.push(["input[name=email]", "L'email ne respect pas le format d'une adresse mail"])
         }
 
--- website/views.py
+++ website/views.py
@@ -288,8 +288,7 @@ class Contact(BaseViewFrontEnd):
         }[client_type]()
 
         # get ip
-        # ip = self.request.headers["cf-connecting-ip"]
-        ip = "0.0.0.0"
+        ip = self.request.headers.get('CF-Connecting-IP', self.request.META.get('REMOTE_ADDR'))
 
         # Send message
         discord_service = DiscordService()
@@ -324,7 +323,9 @@ class Contact(BaseViewFrontEnd):
                 list_err += ['discord username is not valide']
 
         if email:
-            valide_email = re.match(r'^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$', email)
+            # General Email Regex (RFC 5322 Official Standard)
+            regex = r'(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])'
+            valide_email = re.match(regex, email)
             if valide_email == None:
                 list_err += ['email is not valide']
 

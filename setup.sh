6e80b275114afa3a1a2056a28b4b1181a661380f
lululombard <lululombard@gmail.com>
2020-11-11 20:22:26 -0500

    Fix requirements and add setup file

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python3 -m venv env
+
+source env/bin/activate
+
+pip install -r requirements.txt
+
+python manage.py makemigrations
+
+python manage.py migrate
+
+python manage.py sass website/static/website/scss/ website/static/website/css/

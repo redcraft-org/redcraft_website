f6c565ed0a272a5e7bad45fa7e174310fa0b1819
lululombard <lululombard@gmail.com>
2020-11-12 23:18:44 -0500

    Add domains to allowed hosts

--- redcraft_website/settings.py
+++ redcraft_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['testing.redcraft.org', 'staging.redcraft.org', 'redcraft.org']
 
 
 # Application definition

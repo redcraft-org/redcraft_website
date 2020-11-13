19451b05d25eacaa7ec671c2971f43c14feee0a2
lululombard <lululombard@gmail.com>
2020-11-13 01:09:18 -0500

    Update env file format

--- redcraft_website/settings.py
+++ redcraft_website/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['testing.redcraft.org', 'staging.redcraft.org', 'redcraft.org']
+ALLOWED_HOSTS = ['localhost', 'testing.redcraft.org', 'staging.redcraft.org', 'redcraft.org']
 
 
 # Application definition

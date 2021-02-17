4de2b9d259cf4dc1fb8d2a37d63eef0ce48a8298
Codelta <valentin.pretat@he-arc.ch>
2021-02-17 16:21:22 +0100

    added ; in js files

--- website/static/website/js/browser-not-supported.js
+++ website/static/website/js/browser-not-supported.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
     function checkBrowserNotSupported() {
-        let support = new Mooncake(function () {addSupport()})
+        let support = new Mooncake(function () {addSupport()});
     }
 
     function addSupport() {
-        alert("Done")
-        document.querySelector("body").style.fontFamily = "'Comic Neue', cursive"
+        alert("Done");
+        document.querySelector("body").style.fontFamily = "'Comic Neue', cursive";
     }
 
-    checkBrowserNotSupported()
+    checkBrowserNotSupported();
 })

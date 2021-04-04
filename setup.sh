3b467be524865d5d6c162c2b86fb91c1c9f56ea1
lululombard <lululombard@gmail.com>
2021-04-04 13:08:45 -0400

    Remove makemigrations

--- setup.sh
+++ setup.sh
@@ -12,8 +12,6 @@ source env/bin/activate
 
 pip install -r requirements.txt
 
-python manage.py makemigrations
-
 python manage.py migrate
 
 python manage.py sass website/static/website/scss/ website/static/website/css/
